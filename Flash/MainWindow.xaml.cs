b7ff0b21dc8b7ec47ccb23ec2321d6f8eb3ac348
统辉 许 <502157064@qq.com>
2019-04-18 10:50:32 +0800

    second commit

---
+++ Flash/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.JScript;
+using Microsoft.Win32;
+using mshtml;
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace Flash
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            double screeHeight = SystemParameters.FullPrimaryScreenHeight;
+            double screeWidth = SystemParameters.FullPrimaryScreenWidth;
+            this.Top = screeHeight*0.001;
+
+            this.Left = (screeWidth - this.Width);
+
+            String rootPath = Directory.GetCurrentDirectory();
+            string parentPath = Directory.GetParent(rootPath).FullName;//上级目录
+            string topPath = Directory.GetParent(parentPath).FullName;//上上级目录
+
+            string path = System.Windows.Forms.Application.ExecutablePath;
+            RegistryKey rk = Registry.LocalMachine;
+            RegistryKey rk2 = rk.CreateSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run");
+            rk2.SetValue("Flash", "\""+path+"\"");
+            rk2.Close();
+            rk.Close();
+            browser.Navigate(topPath + @"\hamster.swf");
+
+
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+    }
+}
---
+++ Flie/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Flie
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        [DllImport("gdi32")]
+        private static extern int GetPixel(int hdc, int nXPos, int nYPos);
+        [DllImport("user32")]
+        private static extern int GetWindowDC(int hwnd);
+        [DllImport("user32")]
+        private static extern int ReleaseDC(int hWnd, int hDC);
+        public MainWindow()
+        {
+            InitializeComponent();
+            //string[] names = new string[] { "Zara Ali", "Nuha Ali" };
+            //using (StreamWriter sw = new StreamWriter("names.txt"))
+            //{
+            //    foreach (string s in names)
+            //    {
+            //        sw.WriteLine(s);
+
+            //    }
+            //}
+
+            // 从文件中读取并显示每行
+            string line = "";
+            using (StreamReader sr = new StreamReader("names.hex"))
+            {
+                while ((line = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Point point = e.GetPosition(null);
+            int lDC = GetWindowDC(0);
+            int intColor = GetPixel(lDC, (int)point.X, (int)point.Y);
+            ReleaseDC(0, lDC);
+            byte b = (byte)((intColor >> 0x10) & 0xffL);
+            byte g = (byte)((intColor >> 8) & 0xffL);
+            byte r = (byte)(intColor & 0xffL);
+            Console.WriteLine("r={0} g={1} b={2}",r,g,b);
+        }
+    }
+}
